f62a52d4b5f2eeaa0b27217d0f69e9af589294dc
Alaikum <carangiadamo@gmail.com>
2022-09-25 12:58:46 +0200

    other style

--- Prima versione/index.php
+++ Prima versione/index.php
@@ -8,21 +8,22 @@
     <script src='https://cdn.jsdelivr.net/npm/vue@2.7.8/dist/vue.js'></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
     <?php include __DIR__ . '/../db/dischi.php'; ?>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/css.css">
 
     <title>Dischi PHP Versione 1</title>
 </head>
 
 <body>
-    <div id='app' class="container p-3 sfondo">
-
+    <div class="ciao">wewRewe</div>
+    <div id='app' class="container p-3 ">
 
         <ul >
-            <li class="row g-5">
+            <li class="row g-5 gap-1 ">
                 <?php foreach ($dischi as $gruppo) {
-                ?><ul class="col_">
-                        <li>
+                ?><ul class="col_ rounded">
+                        <li class="sfondo">
                             <img class="card-img-top" src="<?= $gruppo['poster'] ?>" alt="">
                             <div class="card-body text-center">
                                 <h4>
